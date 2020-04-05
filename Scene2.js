52fa8483ffd083bce57df491af5909350d7162c7
reed <reedbertolotti@icloud.com>
2020-04-05 12:07:51 -0700

    finished part 9

--- Scene2.js
+++ Scene2.js
@@ -14,6 +14,11 @@ class Scene2 extends Phaser.Scene
     this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
     this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
 
+    this.enemies = this.physics.add.group();
+    this.enemies.add(this.ship1);
+    this.enemies.add(this.ship2);
+    this.enemies.add(this.ship3);
+
     this.ship1.play("ship1_anim");
     this.ship2.play("ship2_anim");
     this.ship3.play("ship3_anim");
@@ -64,6 +69,16 @@ class Scene2 extends Phaser.Scene
 
     this.projectiles = this.add.group();
 
+    this.physics.add.collider(this.projectiles, this.powerUps, function(projectile, powerUp)
+    {
+      projectile.destroy();
+    });
+
+    this.physics.add.overlap(this.player, this.powerUps, this.pickPowerUp, null, this);
+    this.physics.add.overlap(this.player, this.enemies, this.hurtPlayer, null, this);
+
+    this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, null, this);
+
     this.add.text(20, 20, "Playing game",
       {
         font: "25px Arial",
@@ -71,6 +86,27 @@ class Scene2 extends Phaser.Scene
       });
   }
 
+  update()
+  {
+    this.moveShip(this.ship1, 1);
+    this.moveShip(this.ship2, 2);
+    this.moveShip(this.ship3, 3);
+
+    this.background.tilePositionY -= 0.5;
+
+    this.movePlayerManager();
+
+    if (Phaser.Input.Keyboard.JustDown(this.spacebar))
+    {
+      this.shootBeam();
+    }
+
+    for (let i = 0; i < this.projectiles.getChildren().length; i++)
+    {
+      let beam = this.projectiles.getChildren()[i];
+      beam.update();
+    }
+  }
 
   moveShip(ship, speed)
   {
@@ -103,6 +139,8 @@ class Scene2 extends Phaser.Scene
 
   movePlayerManager()
   {
+    this.player.setVelocity(0);
+
     if (this.cursorKeys.left.isDown)
     {
       this.player.setVelocityX(-gameSettings.playerSpeed);
@@ -110,10 +148,6 @@ class Scene2 extends Phaser.Scene
     else if (this.cursorKeys.right.isDown)
     {
       this.player.setVelocityX(gameSettings.playerSpeed);
-    }
-    else
-    {
-      this.player.setVelocityX(0);
     }
 
     if (this.cursorKeys.up.isDown)
@@ -124,10 +158,6 @@ class Scene2 extends Phaser.Scene
     {
       this.player.setVelocityY(gameSettings.playerSpeed);
     }
-    else
-    {
-      this.player.setVelocityY(0);
-    }
   }
 
   shootBeam()
@@ -135,25 +165,21 @@ class Scene2 extends Phaser.Scene
     let beam = new Beam(this);
   }
 
-  update()
+  pickPowerUp(player, powerUp)
   {
-    this.moveShip(this.ship1, 1);
-    this.moveShip(this.ship2, 2);
-    this.moveShip(this.ship3, 3);
+    powerUp.disableBody(true, true);
+  }
 
-    this.background.tilePositionY -= 0.5;
+  hurtPlayer(player, enemy)
+  {
+    this.resetShipPos(enemy);
+    player.x = config.width / 2 - 8;
+    player.y = config.height - 64;
+  }
 
-    this.movePlayerManager();
-
-    if (Phaser.Input.Keyboard.JustDown(this.spacebar))
-    {
-      this.shootBeam();
-    }
-
-    for (let i = 0; i < this.projectiles.getChildren().length; i++)
-    {
-      let beam = this.projectiles.getChildren()[i];
-      beam.update();
-    }
+  hitEnemy(projectile, enemy)
+  {
+    projectile.destroy();
+    this.resetShipPos(enemy);
   }
 }
